02ca4a13e0f5fb4b55ce4ef098bc6b893c930908
Devashish-Tyagi <121316736+Devashish-Tyagi@users.noreply.github.com>
2023-10-03 21:50:45 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        System.out.println("Enter a number ");
+            Scanner sc = new Scanner(System.in);
+            {
+                int num = sc.nextInt();
+                if(num%2==0){
+                    System.out.println("Even number ");
+                }
+                else {
+                    System.out.println("Odd Number ");
+                    
+                }
+            }
+            }
+        }
+    
